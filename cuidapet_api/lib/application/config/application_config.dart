d64462f77c23f642a58324a34de53daf3642d8f8
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-03-14 20:42:12 -0300

    Atualização backEnd

--- cuidapet_api/lib/application/config/application_config.dart
+++ cuidapet_api/lib/application/config/application_config.dart
@@ -1,11 +1,29 @@
-
+import 'package:cuidapet_api/application/config/database_connection_configuration.dart';
 import 'package:dotenv/dotenv.dart';
+import 'package:get_it/get_it.dart';
 
 class ApplicationConfig {
+
+  late DotEnv env;
+
   Future<void> loadConfigApplication() async {
     await _loadEnv();
+    _loadDatabaseConfig();
   }
-  
-  Future<void> _loadEnv() async => DotEnv(includePlatformEnvironment: true)..load();
-}
 
+  Future<void> _loadEnv() async {
+     env = DotEnv(includePlatformEnvironment: true)..load();
+  }
+
+  void _loadDatabaseConfig() {
+    final databaseConfig = DatabaseConnectionConfiguration(
+      host: env['DATABASE_HOST'] ?? env['databaseHost']!,
+      user: env['DATABASE_USER'] ?? env['databaseUser']!,
+      port: int.tryParse(env['DATABASE_PORT`'] ?? env['databasePort']!) ?? 0,
+      password: env['DATABASE_PASSWORD'] ?? env['databasePassword']!,
+      databaseName: env['DATABASE_NAME'] ?? env['databaseName']!,
+    );
+
+    GetIt.I.registerSingleton(databaseConfig);
+  }
+}
