21fcbf2a86778bb4da322ea95ca38ab8488fb6e2
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-03-29 16:20:29 -0300

    Aula Refresh Token

--- cuidapet_api/lib/modules/user/controller/auth_controller.dart
+++ cuidapet_api/lib/modules/user/controller/auth_controller.dart
@@ -9,6 +9,7 @@ import 'package:cuidapet_api/application/logger/i_logger.dart';
 import 'package:cuidapet_api/entities/user.dart';
 import 'package:cuidapet_api/modules/user/view_models/login_view_model.dart';
 import 'package:cuidapet_api/modules/user/view_models/user_confirm_input_model.dart';
+import 'package:cuidapet_api/modules/user/view_models/user_refresh_token_input_model.dart';
 import 'package:cuidapet_api/modules/user/view_models/user_save_input_model.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shelf/shelf.dart';
@@ -35,7 +36,7 @@ class AuthController {
 
       //var user = User();
       User user;
-      
+
       if (!loginViewModel.socialLogin) {
         loginViewModel.loginEmailValidate();
         user = await userService.loginWithEmailPassword(loginViewModel.login,
@@ -48,7 +49,6 @@ class AuthController {
             loginViewModel.avatar,
             loginViewModel.socialType!,
             loginViewModel.socialKey!);
-        
       }
 
       return Response.ok(jsonEncode({
@@ -94,20 +94,41 @@ class AuthController {
   Future<Response> confirmLogin(Request request) async {
     final user = int.parse(request.headers['user']!);
     final supplier = int.tryParse(request.headers['suppller'] ?? '');
-    final token = JwtHelper.generateJWT(user, supplier).replaceAll('Bearer ', '');
+    final token =
+        JwtHelper.generateJWT(user, supplier).replaceAll('Bearer ', '');
 
-    final inputModel = UserConfirmInputModel(userId: user, accessToken: token, data: await request.readAsString());
+    final inputModel = UserConfirmInputModel(
+        userId: user, accessToken: token, data: await request.readAsString());
     final refreshToken = await userService.confirmLogin(inputModel);
 
-     return Response.ok(jsonEncode({
-      'access_token': 'Bearer $token',
-      'refresh_token': refreshToken
-     }));
+    return Response.ok(jsonEncode(
+        {'access_token': 'Bearer $token', 'refresh_token': refreshToken}));
   }
 
   @Route.put('/refresh')
-  Future<Response> refreshToken(Request request) async{
-     return Response.ok(jsonEncode(''));
+  Future<Response> refreshToken(Request request) async {
+    try {
+      final user = int.parse(request.headers['user']!);
+      final supplier = int.tryParse(request.headers['suppller'] ?? '');
+      final accessToken = request.headers['access_token']!;
+      final model = UserRefreshTokenInputModel(
+        user: user,
+        supplier: supplier,
+        accessToken: accessToken,
+        dataRequest: await request.readAsString(),
+      );
+
+      final userRefreshToken = await userService.refreshToken(model);
+
+      return Response.ok(
+        jsonEncode({
+          'access_token': userRefreshToken.accessToken,
+          'refresh_token': userRefreshToken.refreshToken
+        }),
+      );
+    } catch (e) {
+      return Response.internalServerError(body: jsonEncode({'message': 'Erro ao atualizar access token'}));
+    }
   }
 
   Router get router => _$AuthControllerRouter(this);
