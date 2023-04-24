e9ce63ffdb87de89dcf9544b56461d8d5a94b20d
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-04-24 16:08:01 -0300

    Aula Migrando Mysql para Null Safety

--- cuidapet_api/lib/application/config/service_locator_config.config.dart
+++ cuidapet_api/lib/application/config/service_locator_config.config.dart
@@ -4,49 +4,37 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-// ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:cuidapet_api/application/config/database_connection_configuration.dart'
-    as _i5;
-import 'package:cuidapet_api/application/database/i_database_connection.dart'
-    as _i3;
-import 'package:cuidapet_api/application/database/i_database_connection_impl.dart'
-    as _i4;
-import 'package:cuidapet_api/application/logger/i_logger.dart' as _i8;
-import 'package:cuidapet_api/modules/categories/controller/categories_controller.dart'
-    as _i22;
-import 'package:cuidapet_api/modules/categories/data/i_categories_repository.dart'
-    as _i15;
-import 'package:cuidapet_api/modules/categories/data/i_categories_repository_impl.dart'
-    as _i16;
-import 'package:cuidapet_api/modules/categories/service/i_categories_service.dart'
-    as _i17;
-import 'package:cuidapet_api/modules/categories/service/i_categories_service_impl.dart'
-    as _i18;
-import 'package:cuidapet_api/modules/supplier/controller/supplier_controller.dart'
-    as _i21;
-import 'package:cuidapet_api/modules/supplier/data/i_supplier_repository.dart'
-    as _i6;
-import 'package:cuidapet_api/modules/supplier/data/i_supplier_repository_impl.dart'
-    as _i7;
-import 'package:cuidapet_api/modules/supplier/service/i_supplier_service.dart'
-    as _i19;
-import 'package:cuidapet_api/modules/supplier/service/i_supplier_service_impl.dart'
-    as _i20;
-import 'package:cuidapet_api/modules/user/controller/auth_controller.dart'
-    as _i14;
-import 'package:cuidapet_api/modules/user/controller/user_controller.dart'
-    as _i13;
-import 'package:cuidapet_api/modules/user/data/i_user_repository.dart' as _i9;
-import 'package:cuidapet_api/modules/user/data/i_user_repository_impl.dart'
-    as _i10;
-import 'package:cuidapet_api/modules/user/service/i_user_service.dart' as _i11;
-import 'package:cuidapet_api/modules/user/service/i_user_service_impl.dart'
-    as _i12;
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart'
-    as _i2; // ignore_for_file: unnecessary_lambdas
-
+// ignore_for_file: unnecessary_lambdas
 // ignore_for_file: lines_longer_than_80_chars
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+
+import '../../modules/categories/controller/categories_controller.dart' as _i22;
+import '../../modules/categories/data/i_categories_repository.dart' as _i15;
+import '../../modules/categories/data/i_categories_repository_impl.dart'
+    as _i16;
+import '../../modules/categories/service/i_categories_service.dart' as _i17;
+import '../../modules/categories/service/i_categories_service_impl.dart'
+    as _i18;
+import '../../modules/supplier/controller/supplier_controller.dart' as _i21;
+import '../../modules/supplier/data/i_supplier_repository.dart' as _i6;
+import '../../modules/supplier/data/i_supplier_repository_impl.dart' as _i7;
+import '../../modules/supplier/service/i_supplier_service.dart' as _i19;
+import '../../modules/supplier/service/i_supplier_service_impl.dart' as _i20;
+import '../../modules/user/controller/auth_controller.dart' as _i14;
+import '../../modules/user/controller/user_controller.dart' as _i13;
+import '../../modules/user/data/i_user_repository.dart' as _i9;
+import '../../modules/user/data/i_user_repository_impl.dart' as _i10;
+import '../../modules/user/service/i_user_service.dart' as _i11;
+import '../../modules/user/service/i_user_service_impl.dart' as _i12;
+import '../database/i_database_connection.dart' as _i3;
+import '../database/i_database_connection_impl.dart' as _i4;
+import '../logger/i_logger.dart' as _i8;
+import 'database_connection_configuration.dart' as _i5;
+
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
   _i1.GetIt init({
