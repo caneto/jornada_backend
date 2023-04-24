ea64aa5d15b8cdb6f057f60e42d8b8ddaeb83503
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-04-24 17:01:55 -0300

    Aula Agendando serviço em um fornecedor

--- cuidapet_api/lib/modules/schedule/controller/schedule_controller.dart
+++ cuidapet_api/lib/modules/schedule/controller/schedule_controller.dart
@@ -6,11 +6,35 @@ import 'package:injectable/injectable.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import '../service/i_schedule_service.dart';
+import '../view_models/schedule_save_input_model.dart';
+
 
 part 'schedule_controller.g.dart';
 
 @Injectable()
 class ScheduleController {
-  
+  final IScheduleService service;
+  final ILogger log;
+
+  ScheduleController({
+    required this.service,
+    required this.log,
+  });
+
+  @Route.post('/')
+  Future<Response> scheduleServices(Request request) async {
+    try {
+      final userId = int.parse(request.headers['user']!);
+      final inputModel = ScheduleSaveInputModel(
+          userId: userId, dataRequest: await request.readAsString());
+      await service.scheduleService(inputModel);
+      return Response.ok(jsonEncode({}));
+    } catch (e, s) {
+      log.error('Erro ao salvar agendamento', e, s);
+      return Response.internalServerError();
+    }
+  }
+
   Router get router => _$ScheduleControllerRouter(this);
 }
--- cuidapet_api/lib/modules/schedule/service/i_schedule_service_impl.dart
+++ cuidapet_api/lib/modules/schedule/service/i_schedule_service_impl.dart
@@ -1,8 +1,35 @@
+import 'package:cuidapet_api/entities/schedule.dart';
+import 'package:cuidapet_api/entities/schedule_supplier_service.dart';
+import 'package:cuidapet_api/entities/supplier.dart';
+import 'package:cuidapet_api/entities/supplier_service.dart';
+import 'package:cuidapet_api/modules/schedule/data/i_schedule_repository.dart';
+import 'package:cuidapet_api/modules/schedule/view_models/schedule_save_input_model.dart';
 import 'package:injectable/injectable.dart';
 
 import './i_schedule_service.dart';
 
 @LazySingleton(as: IScheduleService)
 class IScheduleServiceImpl implements IScheduleService {
+  final IScheduleRepository repository;
+  
+  IScheduleServiceImpl({
+    required this.repository,
+  });
 
+  @override
+  Future<void> scheduleService(ScheduleSaveInputModel model) async {
+    final schedule = Schedule(
+        scheduleDate: model.scheduleDate,
+        name: model.name,
+        petName: model.petName,
+        supplier: Supplier(id: model.supplierId),
+        status: 'P',
+        userId: model.userId,
+        services: model.services
+            .map((e) => ScheduleSupplierService(
+                  service: SupplierService(id: e),
+                ))
+            .toList());
+    await repository.save(schedule);
+  }
 }
